c49d18253cc727e9ddffa5ea9345a8c7f4c4a083
lucas <lucas.alves.supus@outlook.com>
2023-04-05 02:21:05 -0300

    feat: add all images

--- src/server.ts
+++ src/server.ts
@@ -15,16 +15,6 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = 5000;
 
-// ProductModel.create({
-//   name: "Lipikar Loção Hidratante",
-//   price: 122.91,
-//   description:
-//     "Maravilhoso hidratante corporal com efeito calmante. Sua fórmula contém manteiga de karité e glicerina para hidratar a pele e Alta concentração de Água Termal de La Roche-Posay.",
-//   category: "Cosméticos",
-//   image:
-//     "https://raw.githubusercontent.com/lulucasalves/mypharma-back/main/.github/lipikar.webp",
-// });
-
 app.listen(PORT, () => {
   console.log(`server is running in port ${PORT}!`);
 });
